62e84829e929fd3e2de8da743ddc52ebf9dddff5
berryStraw <marius1357975@gmail.com>
2022-10-30 16:08:01 +0000

    knight piece change

--- src/components/knightSearch/KImage.js
+++ src/components/knightSearch/KImage.js
@@ -3,7 +3,7 @@ const KImage=(props)=>{
     
 
     return(
-        <img src={require('../../styles/knight.png' ) }alt="Knight chess pieace" width="80" height="100%"/> 
+        <img src={require('../../styles/knight.png' ) }alt="Knight chess pieace" width="60" height="90%"/> 
     );
 }
 
