d6dc9a027dfff59e71b82d6d115fc17c81cc3de5
berryStraw <marius1357975@gmail.com>
2022-10-30 15:37:18 +0000

    routing change

--- src/App.js
+++ src/App.js
@@ -9,8 +9,10 @@ function App() {
       <Nav/>
       <Routes>
         <Route path='/' element={<Knight/>}/>
+        <Route path='' element={<Knight/>}/>
         <Route path='/knight' element={<Knight/>}/>
         <Route path='/reactSearchAlgorithms/' element={<Knight/>}/>
+        <Route path='/reactSearchAlgorithms' element={<Knight/>}/>
       </Routes>
     </BrowserRouter>
   );
